fb057299ea2ee0f11c404d2f7f7a9a18884d3085
abdallah <abdallah.salah9700@gmail.com>
2019-07-24 15:13:40 +0200

    able to create post with dynamic cateogory

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
@@ -28,7 +29,10 @@ class AdminPostsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    { return view('admin.posts.create');
+    {
+        $categories = Category::pluck('name','id')->all();
+
+        return view('admin.posts.create',compact('categories'));
     }
 
     /**
